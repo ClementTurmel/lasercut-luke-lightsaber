2ae256f9bedd3ef5e3e6d960b44bdf50159e1464
Clément TURMEL <clemturmel@gmail.com>
2020-08-16 20:19:15 +0200

    add a blinking mode, useable through route

--- code/nodemcu_http_lightsaber/nodemcu_http_lightsaber.ino
+++ code/nodemcu_http_lightsaber/nodemcu_http_lightsaber.ino
@@ -17,13 +17,26 @@ const char* password = "password";
 #define NUMPIXELS        1
 #define KYBER_CRYSTAL    0
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, LED_PIN, NEO_GRB + NEO_KHZ800);
-// *********** **CONST ***************
-#define RED_24B          0xFF0000
-#define ORANGE_24B       0xFF8000
-#define GREEN_24B        0x00FF00
+// ************* CONST ***************
+#define RED_32B          0xFF0000
+#define ORANGE_32B       0xFF8000
+#define GREEN_32B        0x00FF00
 // ***********************************
 
+int red    = 0;
+int green  = 255;
+int blue   = 0;
+
+int brightnessMax   = 100;
+int brightnessMin   = 20;
+int brigthnessSpeed = 15;
+
+
+
+
 ESP8266WebServer server(80);
+
+
 
 void handleRoot(){
  server.send(200, "text/html", 
@@ -49,6 +62,7 @@ void handleRoot(){
         "const Http = new XMLHttpRequest();"
 	"var  url = 'http://192.168.1.13';"
 	"url +='/changeColor' +'?r='+ r +'&g='+ g +'&b='+ b;"
+  
 	"console.log(url);"
         "Http.open('GET', url);"
         "Http.send();"
@@ -70,9 +84,9 @@ void handleRoot(){
 }
 
 void changeColor(){
-    int red = server.arg(0).toInt();
-    int green = server.arg(1).toInt();
-    int blue = server.arg(2).toInt();
+    red = server.arg(0).toInt();
+    green = server.arg(1).toInt();
+    blue = server.arg(2).toInt();
 
     Serial.print("Red : ");
     Serial.println(red);
@@ -89,6 +103,13 @@ void changeColor(){
     handleRoot();
 }
 
+void changeBlink(){
+    brightnessMin   = server.arg(0).toInt();
+    brightnessMax   = server.arg(1).toInt();
+    brigthnessSpeed = server.arg(2).toInt();
+    handleRoot();
+}
+
 void setup(void) {
   Serial.begin(115200);
   pixels.begin(); // This initializes the NeoPixel library.
@@ -100,7 +121,7 @@ void setup(void) {
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
-    pixels.setPixelColor(KYBER_CRYSTAL, ORANGE_24B);
+    pixels.setPixelColor(KYBER_CRYSTAL, ORANGE_32B);
     pixels.show();  
   }
   Serial.println("");
@@ -109,7 +130,7 @@ void setup(void) {
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
   
-  pixels.setPixelColor(KYBER_CRYSTAL, GREEN_24B);
+  pixels.setPixelColor(KYBER_CRYSTAL, GREEN_32B);
   pixels.show();  
 
   if (MDNS.begin("esp8266")) {
@@ -118,12 +139,30 @@ void setup(void) {
   // Route
   server.on("/", handleRoot);
   server.on("/changeColor", changeColor);
+  server.on("/changeBlink", changeBlink);
   
   server.begin();
   Serial.println("HTTP server started");
   Serial.println("Starting Loop");
 }
 
+void blinkColor(){
+  for (float i = brightnessMax; i >= brightnessMin; i--) {
+    applyBrigthness(i);
+  }
+  for (float i = brightnessMin ; i <= brightnessMax; i++) {
+    applyBrigthness(i);
+  }
+}
+
+void applyBrigthness(float i){
+    pixels.setPixelColor(KYBER_CRYSTAL, pixels.Color(red*(i/100), green*(i/100), blue*(i/100))); 
+    pixels.show();
+    delay(brigthnessSpeed);
+    server.handleClient();
+}
+
 void loop() {
   server.handleClient();
+  blinkColor();
 }
